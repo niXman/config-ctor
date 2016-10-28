75af9ed0dcc2ed074383cb403e67b75856fc76ac
niXman <i.nixman@autistici.org>
2016-10-28 18:05:25 +0300

    reformat

--- include/config-ctor/config-ctor.hpp
+++ include/config-ctor/config-ctor.hpp
@@ -1,6 +1,6 @@
 
-#ifndef _config_ctor_hpp
-#define _config_ctor_hpp
+#ifndef __config_ctor__config_ctor_hpp
+#define __config_ctor__config_ctor_hpp
 
 #include <cstdint>
 #include <string>
@@ -29,75 +29,75 @@ namespace {
 // for other types (strings)
 template<bool ok>
 struct get_concrete_value {
-	template<typename T>
-	static T get(const char *key, const boost::property_tree::ptree &ini, const char *default_value) {
-		return default_value != nullptr
-			? ini.get<T>(key, default_value)
-			: ini.get<T>(key)
-		;
-	}
+    template<typename T>
+    static T get(const char *key, const boost::property_tree::ptree &ini, const char *default_value) {
+        return default_value != nullptr
+           ? ini.get<T>(key, default_value)
+           : ini.get<T>(key)
+        ;
+    }
 };
 
 // specialization for arithmetic types
 template<>
 struct get_concrete_value<true> {
-	template<typename T>
-	static T get(const char *key, const boost::property_tree::ptree &ini, const char *default_value) {
-		// handle bool types
-		if ( std::is_same<T, bool>::value ) {
-			return default_value != nullptr
-				? ini.get<bool>(key, (std::strcmp(default_value, "true") == 0))
-				: ini.get<bool>(key)
-			;
-		}
+    template<typename T>
+    static T get(const char *key, const boost::property_tree::ptree &ini, const char *default_value) {
+        // handle bool types
+        if (std::is_same<T, bool>::value) {
+            return default_value != nullptr
+               ? ini.get<bool>(key, (std::strcmp(default_value, "true") == 0))
+               : ini.get<bool>(key)
+            ;
+        }
 
-		// handle other arithmetic types
-		std::string val = default_value != nullptr
-			? ini.get<std::string>(key, default_value)
-			: ini.get<std::string>(key)
-		;
-		if ( val.empty() ) return T{};
+        // handle other arithmetic types
+        std::string val = default_value != nullptr
+            ? ini.get<std::string>(key, default_value)
+            : ini.get<std::string>(key)
+        ;
+        if (val.empty()) return T{};
 
-		if ( (std::is_signed<T>::value || std::is_unsigned<T>::value) && !std::is_floating_point<T>::value ) {
-			std::size_t mult = 1u;
-			switch ( val.back() ) {
-				case 't': case 'T': mult *= 1024u;
-				case 'g': case 'G': mult *= 1024u;
-				case 'm': case 'M': mult *= 1024u;
-				case 'k': case 'K': mult *= 1024u;
+        if ((std::is_signed<T>::value || std::is_unsigned<T>::value) && !std::is_floating_point<T>::value) {
+            std::size_t mult = 1u;
+            switch (val.back()) {
+                case 't': case 'T': mult *= 1024u;
+                case 'g': case 'G': mult *= 1024u;
+                case 'm': case 'M': mult *= 1024u;
+                case 'k': case 'K': mult *= 1024u;
 
-				val.pop_back();
-			}
+                val.pop_back();
+            }
 
-			return static_cast<T>((std::is_signed<T>::value ? std::stol(val) : std::stoul(val)) * mult);
-		}
+            return static_cast<T>((std::is_signed<T>::value ? std::stol(val) : std::stoul(val)) * mult);
+        }
 
-		return static_cast<T>(std::stod(val));
-	}
+        return static_cast<T>(std::stod(val));
+    }
 };
 
 } // anon ns
 
 template<typename T>
 static T get_value(const char *key, const boost::property_tree::ptree &cfg, const char *default_value) {
-	using TT = typename std::remove_cv<T>::type;
-	return get_concrete_value<std::is_arithmetic<TT>::value>::template get<TT>(key, cfg, default_value);
+    using TT = typename std::remove_cv<T>::type;
+    return get_concrete_value<std::is_arithmetic<TT>::value>::template get<TT>(key, cfg, default_value);
 }
 
 template<bool ok>
 struct print_value {
-	template<typename T>
-	static void print(const T &v, std::ostream &os) {
-		os << '\"' << v << '\"';
-	}
+    template<typename T>
+    static void print(const T &v, std::ostream &os) {
+        os << '\"' << v << '\"';
+    }
 };
 
 template<>
 struct print_value<true> {
-	template<typename T>
-	static void print(const T &v, std::ostream &os) {
-		os << std::boolalpha << v;
-	}
+    template<typename T>
+    static void print(const T &v, std::ostream &os) {
+        os << std::boolalpha << v;
+    }
 };
 
 } // ns construct_config
@@ -105,9 +105,9 @@ struct print_value<true> {
 /***************************************************************************/
 
 #define _CONSTRUCT_CONFIG__WRAP_SEQUENCE_X(...) \
-	((__VA_ARGS__)) _CONSTRUCT_CONFIG__WRAP_SEQUENCE_Y
+    ((__VA_ARGS__)) _CONSTRUCT_CONFIG__WRAP_SEQUENCE_Y
 #define _CONSTRUCT_CONFIG__WRAP_SEQUENCE_Y(...) \
-	((__VA_ARGS__)) _CONSTRUCT_CONFIG__WRAP_SEQUENCE_X
+    ((__VA_ARGS__)) _CONSTRUCT_CONFIG__WRAP_SEQUENCE_X
 
 #define _CONSTRUCT_CONFIG__WRAP_SEQUENCE_X0
 #define _CONSTRUCT_CONFIG__WRAP_SEQUENCE_Y0
@@ -115,126 +115,126 @@ struct print_value<true> {
 /***************************************************************************/
 
 #define _CONSTRUCT_CONFIG__GENERATE_MEMBERS(unused, data, idx, elem) \
-	BOOST_PP_TUPLE_ELEM(0, elem) /* type */ BOOST_PP_TUPLE_ELEM(1, elem) /* var name */ ;
+    BOOST_PP_TUPLE_ELEM(0, elem) /* type */ BOOST_PP_TUPLE_ELEM(1, elem) /* var name */ ;
 
 #define _CONSTRUCT_CONFIG__INIT_MEMBERS_WITH_DEFAULT(...) \
-	,BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(2, __VA_ARGS__))
+    ,BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(2, __VA_ARGS__))
 
 #define _CONSTRUCT_CONFIG__INIT_MEMBERS_WITHOUT_DEFAULT(...) \
-	,nullptr
+    ,nullptr
 
 #define _CONSTRUCT_CONFIG__INIT_MEMBERS(unused, data, idx, elem) \
-	BOOST_PP_COMMA_IF(idx) \
-		::construct_config::get_value<BOOST_PP_TUPLE_ELEM(0, elem)>( \
-			 BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)) \
-			,cfg \
-			BOOST_PP_IF( \
-				 BOOST_PP_GREATER_EQUAL(BOOST_PP_TUPLE_SIZE(elem), 3) \
-				,_CONSTRUCT_CONFIG__INIT_MEMBERS_WITH_DEFAULT \
-				,_CONSTRUCT_CONFIG__INIT_MEMBERS_WITHOUT_DEFAULT \
-			)(elem) \
-		)
+    BOOST_PP_COMMA_IF(idx) \
+        ::construct_config::get_value<BOOST_PP_TUPLE_ELEM(0, elem)>( \
+             BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)) \
+            ,cfg \
+            BOOST_PP_IF( \
+                 BOOST_PP_GREATER_EQUAL(BOOST_PP_TUPLE_SIZE(elem), 3) \
+                ,_CONSTRUCT_CONFIG__INIT_MEMBERS_WITH_DEFAULT \
+                ,_CONSTRUCT_CONFIG__INIT_MEMBERS_WITHOUT_DEFAULT \
+            )(elem) \
+        )
 
 #define _CONSTRUCT_CONFIG__ENUM_MEMBERS(unused, data, idx, elem) \
-	os << BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)) "="; \
-	::construct_config::print_value< \
-		std::is_arithmetic<decltype(BOOST_PP_TUPLE_ELEM(1, elem))>::value \
-	>::print(BOOST_PP_TUPLE_ELEM(1, elem), os); \
-	os << std::endl;
+    os << BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)) "="; \
+    ::construct_config::print_value< \
+        std::is_arithmetic<decltype(BOOST_PP_TUPLE_ELEM(1, elem))>::value \
+    >::print(BOOST_PP_TUPLE_ELEM(1, elem), os); \
+    os << std::endl;
 
 /***************************************************************************/
 
 #define _CONSTRUCT_CONFIG__ENUM_WRITE_MEMBERS(unused, data, idx, elem) \
-	ptree.put(BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)), cfg.BOOST_PP_TUPLE_ELEM(1, elem));
+    ptree.put(BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)), cfg.BOOST_PP_TUPLE_ELEM(1, elem));
 
 /***************************************************************************/
 
 #define _CONSTRUCT_CONFIG__GENERATE_STRUCT(fmt, name, seq) \
-	struct name { \
-		BOOST_PP_SEQ_FOR_EACH_I( \
-			 _CONSTRUCT_CONFIG__GENERATE_MEMBERS \
-			,~ \
-			,seq \
-		) \
-		\
-		static name read(std::istream &is) { \
-			boost::property_tree::ptree cfg; \
-			boost::property_tree::read_##fmt(is, cfg); \
-			\
-			return { \
-				BOOST_PP_SEQ_FOR_EACH_I( \
-					 _CONSTRUCT_CONFIG__INIT_MEMBERS \
-					,~ \
-					,seq \
-				) \
-			}; \
-		} \
-		static name read(const std::string &fname) { \
-			boost::property_tree::ptree cfg; \
-			boost::property_tree::read_##fmt(fname, cfg); \
-			\
-			return { \
-				BOOST_PP_SEQ_FOR_EACH_I( \
-					 _CONSTRUCT_CONFIG__INIT_MEMBERS \
-					,~ \
-					,seq \
-				) \
-			}; \
-		} \
-		\
-		static void write(const std::string &fname, const name &cfg) { \
-			boost::property_tree::ptree ptree; \
-			BOOST_PP_SEQ_FOR_EACH_I( \
-				 _CONSTRUCT_CONFIG__ENUM_WRITE_MEMBERS \
-				,~ \
-				,seq \
-			) \
-			boost::property_tree::write_##fmt(fname, ptree); \
-		} \
-		static void write(std::ostream &os, const name &cfg) { \
-			boost::property_tree::ptree ptree; \
-			BOOST_PP_SEQ_FOR_EACH_I( \
-				 _CONSTRUCT_CONFIG__ENUM_WRITE_MEMBERS \
-				,~ \
-				,seq \
-			) \
-			boost::property_tree::write_##fmt(os, ptree); \
-		} \
-		\
-		void dump(std::ostream &os) const { \
-			BOOST_PP_SEQ_FOR_EACH_I( \
-				 _CONSTRUCT_CONFIG__ENUM_MEMBERS \
-				,~ \
-				,seq \
-			) \
-		} \
-	};
+    struct name { \
+        BOOST_PP_SEQ_FOR_EACH_I( \
+             _CONSTRUCT_CONFIG__GENERATE_MEMBERS \
+            ,~ \
+            ,seq \
+        ) \
+        \
+        static name read(std::istream &is) { \
+            boost::property_tree::ptree cfg; \
+            boost::property_tree::read_##fmt(is, cfg); \
+            \
+            return { \
+                BOOST_PP_SEQ_FOR_EACH_I( \
+                     _CONSTRUCT_CONFIG__INIT_MEMBERS \
+                    ,~ \
+                    ,seq \
+                ) \
+            }; \
+        } \
+        static name read(const std::string &fname) { \
+            boost::property_tree::ptree cfg; \
+            boost::property_tree::read_##fmt(fname, cfg); \
+            \
+            return { \
+                BOOST_PP_SEQ_FOR_EACH_I( \
+                     _CONSTRUCT_CONFIG__INIT_MEMBERS \
+                    ,~ \
+                    ,seq \
+                ) \
+            }; \
+        } \
+        \
+        static void write(const std::string &fname, const name &cfg) { \
+            boost::property_tree::ptree ptree; \
+            BOOST_PP_SEQ_FOR_EACH_I( \
+                 _CONSTRUCT_CONFIG__ENUM_WRITE_MEMBERS \
+                ,~ \
+                ,seq \
+            ) \
+            boost::property_tree::write_##fmt(fname, ptree); \
+        } \
+        static void write(std::ostream &os, const name &cfg) { \
+            boost::property_tree::ptree ptree; \
+            BOOST_PP_SEQ_FOR_EACH_I( \
+                 _CONSTRUCT_CONFIG__ENUM_WRITE_MEMBERS \
+                ,~ \
+                ,seq \
+            ) \
+            boost::property_tree::write_##fmt(os, ptree); \
+        } \
+        \
+        void dump(std::ostream &os) const { \
+            BOOST_PP_SEQ_FOR_EACH_I( \
+                 _CONSTRUCT_CONFIG__ENUM_MEMBERS \
+                ,~ \
+                ,seq \
+            ) \
+        } \
+    };
 
 /***************************************************************************/
 
-#define CONSTRUCT_CONFIG( \
-	 fmt  /* config file format */ \
-	,name /* config struct name */ \
-	,seq  /* sequence of vars */ \
+#define CONSTRUCT_CONFIG(\
+     fmt  /* config file format */ \
+, name /* config struct name */ \
+, seq  /* sequence of vars */ \
 ) \
-	_CONSTRUCT_CONFIG__GENERATE_STRUCT( \
-		 fmt \
-		,name \
-		,BOOST_PP_CAT(_CONSTRUCT_CONFIG__WRAP_SEQUENCE_X seq, 0) \
-	)
+    _CONSTRUCT_CONFIG__GENERATE_STRUCT( \
+         fmt \
+        ,name \
+        ,BOOST_PP_CAT(_CONSTRUCT_CONFIG__WRAP_SEQUENCE_X seq, 0) \
+    )
 
 #define CONSTRUCT_INI_CONFIG(name, seq) \
-	CONSTRUCT_CONFIG(ini, name, seq)
+    CONSTRUCT_CONFIG(ini, name, seq)
 
 #define CONSTRUCT_JSON_CONFIG(name, seq) \
-	CONSTRUCT_CONFIG(json, name, seq)
+    CONSTRUCT_CONFIG(json, name, seq)
 
 #define CONSTRUCT_XML_CONFIG(name, seq) \
-	CONSTRUCT_CONFIG(xml, name, seq)
+    CONSTRUCT_CONFIG(xml, name, seq)
 
 #define CONSTRUCT_INFO_CONFIG(name, seq) \
-	CONSTRUCT_CONFIG(info, name, seq)
+    CONSTRUCT_CONFIG(info, name, seq)
 
 /***************************************************************************/
 
-#endif // _config_ctor_hpp
+#endif // __config_ctor__config_ctor_hpp
--- main.cpp
+++ main.cpp
@@ -4,156 +4,156 @@
 #include <iostream>
 
 #define MY_ASSERT(...) \
-	if ( !(__VA_ARGS__) ) { \
-		std::cerr << "expression error(" __FILE__ ":" BOOST_PP_STRINGIZE(__LINE__) "): \"" #__VA_ARGS__ "\"" << std::endl; \
-		std::abort(); \
-	}
+    if ( !(__VA_ARGS__) ) { \
+        std::cerr << "expression error(" __FILE__ ":" BOOST_PP_STRINGIZE(__LINE__) "): \"" #__VA_ARGS__ "\"" << std::endl; \
+        std::abort(); \
+    }
 
 #define DUMP_CONFIG(os, ss, descr) \
-	os << "[" descr "]:" << std::endl; \
-	os << ss.str() << std::endl;
+    os << "[" descr "]:" << std::endl; \
+    os << ss.str() << std::endl;
 
 /***************************************************************************/
 
 int main() {
-	//////////////////////////////////////////////// ini
-	{
-		CONSTRUCT_INI_CONFIG(
-			config,
-			(int, a)
-			(float, b)
-			(std::string, c)
-		)
+    //////////////////////////////////////////////// ini
+    {
+        CONSTRUCT_INI_CONFIG(
+            config,
+            (int, a)
+            (float, b)
+            (std::string, c)
+        )
 
-		static const int a = 33;
-		static const float b = 44.55;
-		static const std::string c = "some string";
+        static const int a = 33;
+        static const float b = 44.55;
+        static const std::string c = "some string";
 
-		config wcfg;
-		wcfg.a = a;
-		wcfg.b = b;
-		wcfg.c = c;
+        config wcfg;
+        wcfg.a = a;
+        wcfg.b = b;
+        wcfg.c = c;
 
-		std::stringstream ss;
-		config::write(ss, wcfg);
-		DUMP_CONFIG(std::cout, ss, "test 'ini' config dump")
+        std::stringstream ss;
+        config::write(ss, wcfg);
+        DUMP_CONFIG(std::cout, ss, "test 'ini' config dump")
 
-		const config rcfg = config::read(ss);
-		MY_ASSERT(rcfg.a == a);
-		MY_ASSERT(rcfg.b == b);
-		MY_ASSERT(rcfg.c == c);
-	}
-	//////////////////////////////////////////////// json
-	{
-		CONSTRUCT_JSON_CONFIG(
-			config,
-			(double, a)
-			(long, b)
-			(std::uint16_t, c)
-		)
+        const config rcfg = config::read(ss);
+        MY_ASSERT(rcfg.a == a);
+        MY_ASSERT(rcfg.b == b);
+        MY_ASSERT(rcfg.c == c);
+    }
+    //////////////////////////////////////////////// json
+    {
+        CONSTRUCT_JSON_CONFIG(
+            config,
+            (double, a)
+            (long, b)
+            (std::uint16_t, c)
+        )
 
-		static const double a = 33.44;
-		static const long b = 55;
-		static const std::uint16_t c = 66;
+        static const double a = 33.44;
+        static const long b = 55;
+        static const std::uint16_t c = 66;
 
-		config wcfg;
-		wcfg.a = a;
-		wcfg.b = b;
-		wcfg.c = c;
+        config wcfg;
+        wcfg.a = a;
+        wcfg.b = b;
+        wcfg.c = c;
 
-		std::stringstream ss;
-		config::write(ss, wcfg);
-		DUMP_CONFIG(std::cout, ss, "test 'json' config dump")
+        std::stringstream ss;
+        config::write(ss, wcfg);
+        DUMP_CONFIG(std::cout, ss, "test 'json' config dump")
 
-		const config rcfg = config::read(ss);
-		MY_ASSERT(rcfg.a == a);
-		MY_ASSERT(rcfg.b == b);
-		MY_ASSERT(rcfg.c == c);
-	}
-	//////////////////////////////////////////////// xml
-	{
-		CONSTRUCT_XML_CONFIG(
-			config,
-			(double, a)
-			(long, b)
-			(std::uint16_t, c)
-		)
+        const config rcfg = config::read(ss);
+        MY_ASSERT(rcfg.a == a);
+        MY_ASSERT(rcfg.b == b);
+        MY_ASSERT(rcfg.c == c);
+    }
+    //////////////////////////////////////////////// xml
+    {
+        CONSTRUCT_XML_CONFIG(
+            config,
+            (double, a)
+            (long, b)
+            (std::uint16_t, c)
+        )
 
-		static const double a = 33.44;
-		static const long b = 55;
-		static const std::uint16_t c = 66;
+        static const double a = 33.44;
+        static const long b = 55;
+        static const std::uint16_t c = 66;
 
-		config wcfg;
-		wcfg.a = a;
-		wcfg.b = b;
-		wcfg.c = c;
+        config wcfg;
+        wcfg.a = a;
+        wcfg.b = b;
+        wcfg.c = c;
 
-		std::stringstream ss;
-		config::write(ss, wcfg);
-		DUMP_CONFIG(std::cout, ss, "test 'xml' config dump")
+        std::stringstream ss;
+        config::write(ss, wcfg);
+        DUMP_CONFIG(std::cout, ss, "test 'xml' config dump")
 
-		const config rcfg = config::read(ss);
-		MY_ASSERT(rcfg.a == a);
-		MY_ASSERT(rcfg.b == b);
-		MY_ASSERT(rcfg.c == c);
-	}
-	//////////////////////////////////////////////// info
-	{
-		CONSTRUCT_INFO_CONFIG(
-			config,
-			(double, a)
-			(long, b)
-			(std::uint16_t, c)
-		)
+        const config rcfg = config::read(ss);
+        MY_ASSERT(rcfg.a == a);
+        MY_ASSERT(rcfg.b == b);
+        MY_ASSERT(rcfg.c == c);
+    }
+    //////////////////////////////////////////////// info
+    {
+        CONSTRUCT_INFO_CONFIG(
+            config,
+            (double, a)
+            (long, b)
+            (std::uint16_t, c)
+        )
 
-		static const double a = 33.44;
-		static const long b = 55;
-		static const std::uint16_t c = 66;
+        static const double a = 33.44;
+        static const long b = 55;
+        static const std::uint16_t c = 66;
 
-		config wcfg;
-		wcfg.a = a;
-		wcfg.b = b;
-		wcfg.c = c;
+        config wcfg;
+        wcfg.a = a;
+        wcfg.b = b;
+        wcfg.c = c;
 
-		std::stringstream ss;
-		config::write(ss, wcfg);
-		DUMP_CONFIG(std::cout, ss, "test 'info' config dump")
+        std::stringstream ss;
+        config::write(ss, wcfg);
+        DUMP_CONFIG(std::cout, ss, "test 'info' config dump")
 
-		const config rcfg = config::read(ss);
-		MY_ASSERT(rcfg.a == a);
-		MY_ASSERT(rcfg.b == b);
-		MY_ASSERT(rcfg.c == c);
-	}
-	//////////////////////////////////////////////// default value test
-	{
-		CONSTRUCT_INI_CONFIG(
-			config,
-			(int, a)
-			(bool, b, false)
-			(float, c, 22)
-			(std::string, d)
-			(std::string, e, /path/to/some/file.ext)
-			(long, f)
-		)
+        const config rcfg = config::read(ss);
+        MY_ASSERT(rcfg.a == a);
+        MY_ASSERT(rcfg.b == b);
+        MY_ASSERT(rcfg.c == c);
+    }
+    //////////////////////////////////////////////// default value test
+    {
+        CONSTRUCT_INI_CONFIG(
+            config,
+            (int, a)
+            (bool, b, false)
+            (float, c, 22)
+            (std::string, d)
+            (std::string, e, /path/to/some/file.ext)
+            (long, f)
+        )
 
-		static const int a = 33;
-		static const std::string d = "string";
+        static const int a = 33;
+        static const std::string d = "string";
 
-		std::stringstream ss;
-		ss << "a=" << a << std::endl
-			<< "d=" << d << std::endl
-			<< "f=64k" << std::endl;
+        std::stringstream ss;
+        ss << "a=" << a << std::endl
+           << "d=" << d << std::endl
+           << "f=64k" << std::endl;
 
-		const config rcfg = config::read(ss);
-		MY_ASSERT(rcfg.a == a);
-		MY_ASSERT(rcfg.b == false);
-		MY_ASSERT(rcfg.c == 22);
-		MY_ASSERT(rcfg.d == d);
-		MY_ASSERT(rcfg.e == "/path/to/some/file.ext");
-		MY_ASSERT(rcfg.f == 64*1024);
-	}
-	
-	return EXIT_SUCCESS;
+        const config rcfg = config::read(ss);
+        MY_ASSERT(rcfg.a == a);
+        MY_ASSERT(rcfg.b == false);
+        MY_ASSERT(rcfg.c == 22);
+        MY_ASSERT(rcfg.d == d);
+        MY_ASSERT(rcfg.e == "/path/to/some/file.ext");
+        MY_ASSERT(rcfg.f == 64*1024);
+    }
+
+    return EXIT_SUCCESS;
 }
 
 /***************************************************************************/
