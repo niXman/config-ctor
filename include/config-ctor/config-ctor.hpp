16a4db12ff7af979f8ca2bc7afc1fd8c0bc168a8
niXman <niXman@users.noreply.github.com>
2017-08-30 12:44:59 +0300

    Update config-ctor.hpp

--- include/config-ctor/config-ctor.hpp
+++ include/config-ctor/config-ctor.hpp
@@ -58,15 +58,16 @@
 
 #ifdef _WIN32
 #   include <windows.h>
-#else
-#   include <unistd.h>
-#   include <stdlib.h>
-#endif
+#endif // _WIN32
 
 #ifdef _MSC_VER
 #   include <direct.h>
 #   include <process.h>
+#else
+#   include <unistd.h>
 #endif // _MSC_VER
+
+#include <stdlib.h>
 
 /***************************************************************************/
 
